bd06ad13e7d28b5773fe70f2d957f5fb97d6b315
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-12-15 09:46:06 -0500

    Added Product.py

---
+++ Product.py
@@ -0,0 +1,13 @@
+import csv
+
+class Product : 
+
+    def __init__(self , code , name , type , price , quantity ):
+
+        self.code = code
+        self.name = name
+        self.type = type
+        self.price = price
+        self.quantity = quantity
+
+        
