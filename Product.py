9e79c884cf6edaa9a81405163b6d7f34351bb96e
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-12-15 10:00:33 -0500

    Added code in Product.py

--- Product.py
+++ Product.py
@@ -10,4 +10,62 @@ class Product :
         self.price = price
         self.quantity = quantity
 
-        
+    def __str__(self):
+
+        return f"\nCode : {self.code} , Name : {self.name} , Type : {self.type} , Price : {self.price} , Quantity : {self.quantity}"
+
+
+class Data : 
+
+    @staticmethod  
+
+    def getData(filename):
+
+        products = [ ]
+
+        with open(filename , "r") as file :
+
+            reader = csv.reader(file)
+
+            for row in reader:
+
+                p = Product(row[0] , row[1] , row[2] , row[3] , row[4])
+                products.append(p)
+
+            return products
+
+    @staticmethod
+
+    def writeData(filename , rows):
+
+        with open(filename , "w" , newline ='') as file:
+
+            writer = csv.writer(rows)  
+
+class Store:
+
+    def __init__(self):
+
+        self.products = Data.getData('products_sales.csv')
+
+    def addProduct(self , product):
+
+        self.products.append(product)
+
+    def getProducts(self):
+
+        return self.products
+
+    def findProduct(self, type):
+
+        p = None 
+
+        for product in self.products:
+
+            if product.type == type :
+
+                 p = product
+                 break   
+        return p
+    
+    
